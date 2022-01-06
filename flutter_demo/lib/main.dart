fc5da4987d1f0b3e2abac696b0939b8efa52ca64
chunfeng.liu <chunfeng.liu@innocellence.com>
2022-01-06 17:27:48 +0800

    feat: add route and login page; use navigator methods

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_demo/src/screens/login.dart';
+import 'package:flutter_demo/src/screens/randomWords.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,97 +9,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void onLogin(Credentials) {
+    print('onLogin');
+    print(Credentials);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '名字生成器',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = Set<WordPair>();
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () => {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        })
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      final tiles = _saved.map((pair) {
-        return ListTile(
-          title: Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      });
-
-      final divided = ListTile.divideTiles(
-        context: context,
-        tiles: tiles,
-      ).toList();
-
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text('过滤喜欢的条目'),
-          ),
-          body: ListView(children: divided));
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
-          ]),
-      body: _buildSuggestions(),
-    );
+        title: '名字生成器',
+        // 该设置未生效
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginScreen(onLogin: onLogin),
+          '/random': (context) => RandomWords(),
+        });
   }
 }
---
+++ flutter_demo/lib/src/screens/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Credentials {
+  final String username;
+  final String password;
+
+  Credentials(this.username, this.password);
+}
+
+class LoginScreen extends StatefulWidget {
+  final ValueChanged<Credentials> onLogin;
+
+  const LoginScreen({
+    required this.onLogin,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+          body: Center(
+              child: Card(
+                  child: Container(
+        constraints: BoxConstraints.loose(const Size(600, 600)),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('登录', style: Theme.of(context).textTheme.headline4),
+            TextField(
+              decoration: const InputDecoration(labelText: "用户名"),
+              controller: _usernameController,
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: "密码"),
+              controller: _passwordController,
+              obscureText: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextButton(
+                onPressed: () {
+                  widget.onLogin(Credentials(_usernameController.value.text,
+                      _passwordController.value.text));
+                  Navigator.pushReplacementNamed(context, '/random');
+                },
+                child: const Text('登录'),
+              ),
+            ),
+          ],
+        ),
+      ))));
+}
