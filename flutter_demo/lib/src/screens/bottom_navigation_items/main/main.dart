3fd8f56e0b75a3824cdbc3aa943fc112c4b9f2d9
chunfeng.liu <chunfeng.liu@innocellence.com>
2022-01-07 17:36:30 +0800

    feat: test navigator

--- flutter_demo/lib/src/screens/bottom_navigation_items/main/main.dart
+++ flutter_demo/lib/src/screens/bottom_navigation_items/main/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/src/screens/RandomWords.dart';
 
 class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16.0),
-      child: const Text('mainWidget'),
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => RandomWords()));
+        },
+        child: const Text('跳转'),
+      ),
     );
   }
 }
