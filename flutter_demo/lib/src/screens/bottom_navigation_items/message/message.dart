1c1c42129ecc1902e64afc8e5d82cbdbfc8e7106
chunfeng.liu <chunfeng.liu@innocellence.com>
2022-01-10 15:07:41 +0800

    feat: add ajax demo

--- flutter_demo/lib/src/screens/bottom_navigation_items/message/message.dart
+++ flutter_demo/lib/src/screens/bottom_navigation_items/message/message.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void getHttp() async {
+  try {
+    // var response = await Dio().get('http://www.google.com');
+    var response = await Dio().get("/test?id=12&name=wendu");
+    print(response);
+  } catch (e) {
+    print(e);
+  }
+}
 
 class MessageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16.0),
-      child: const Text('MessageWidget'),
+      child: TextButton(
+        onPressed: () {
+          getHttp();
+        },
+        child: const Text('ajax请求'),
+      ),
     );
   }
 }
