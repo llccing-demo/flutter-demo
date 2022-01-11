f395641bb0986a2d5f74c9bb7bc94a674ca879f0
Bingtao Lu <bingtao.lu@innocellence.com>
2022-01-11 11:33:11 +0800

    lbt 1.11

--- flutter_demo/lib/src/screens/login.dart
+++ flutter_demo/lib/src/screens/login.dart
@@ -22,40 +22,145 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  Widget _buildLoginButton() => MaterialButton(
+        // color: Colors.lightBlue[500],
+        color: Colors.lightBlueAccent[200],
+        textColor: Colors.white,
+        child: Text('登录'),
+        minWidth: 500,
+        height: 50,
+        onPressed: () {
+          if (_checkLogin(_usernameController.text, _passwordController.text)) {
+            print('success');
+            Navigator.pushReplacementNamed(context, '/home');
+          } else {
+            print('fail');
+          }
+        },
+      );
+  bool _checkLogin(username, password) {
+    if (username == "lbt" && password == "123") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Widget _buildUserNameInput() {
+    return TextField(
+      decoration: const InputDecoration(hintText: "请输入登录账号"),
+      obscureText: false,
+      controller: _usernameController,
+      keyboardType: TextInputType.text,
+    );
+  }
+
+  Widget _buildPasswordInput() {
+    return TextField(
+      decoration: const InputDecoration(hintText: "请输入密码"),
+      controller: _passwordController,
+      obscureText: true,
+    );
+  }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-          body: Center(
-              child: Card(
-                  child: Container(
-        constraints: BoxConstraints.loose(const Size(600, 600)),
-        padding: const EdgeInsets.all(8),
+  Widget build(BuildContext context) => _buildLoginWrapper();
+  Widget _buildLoginWrapper() {
+    // return Scaffold(body: Center(child: _buildLoginInputWrapper()));
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildLoginTopWrapper(),
+        _buildLoginInputAndBottomWrapper(),
+      ],
+    );
+  }
+
+  // 输入框容器+底部的容器
+  Widget _buildLoginInputAndBottomWrapper() {
+    return Container(
+        constraints: BoxConstraints.loose(const Size(600, 400)),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              _buildLoginInputWrapper(),
+              _buildLoginBottomWrapper(),
+            ]));
+  }
+
+  //登录 输入框 容器
+  Widget _buildLoginInputWrapper() {
+    return Card(
+        child: Container(
+      constraints: BoxConstraints.loose(const Size(600, 200)),
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildUserNameInput(),
+          _buildPasswordInput(),
+          Padding(padding: const EdgeInsets.all(8), child: _buildLoginButton()),
+        ],
+      ),
+    ));
+  }
+
+  //底部容器
+  Widget _buildLoginBottomWrapper() {
+    return Container(
+      constraints: BoxConstraints.loose(const Size(600, 200)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('登录', style: Theme.of(context).textTheme.headline4),
-            TextField(
-              decoration: const InputDecoration(labelText: "用户名"),
-              controller: _usernameController,
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: "密码"),
-              controller: _passwordController,
-              obscureText: true,
+            Padding(
+              padding: EdgeInsets.only(top: 100),
+              child: Container(
+                  // constraints: BoxConstraints.loose(const Size(600, 10)),
+                  child: Text("AstraZeneca",
+                      style: TextStyle(
+                          color: Colors.purple,
+                          decoration: TextDecoration.none,
+                          fontSize: 16))),
             ),
             Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextButton(
-                onPressed: () {
-                  widget.onLogin(Credentials(_usernameController.value.text,
-                      _passwordController.value.text));
-                  Navigator.pushReplacementNamed(context, '/home');
-                },
-                child: const Text('登录'),
-              ),
-            ),
+              padding: EdgeInsets.only(bottom: 40),
+              child: Container(
+                  constraints: BoxConstraints.loose(const Size(600, 100)),
+                  child: Text('POWERD BY ID 阿斯利康中国',
+                      style: TextStyle(
+                          color: Colors.black54,
+                          decoration: TextDecoration.none,
+                          fontSize: 10))),
+            )
           ],
         ),
-      ))));
+      ),
+    );
+  }
+
+  Widget _buildLoginTopWrapper() {
+    return Container(
+      constraints: BoxConstraints.loose(const Size(600, 200)),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('loginTopBg.webp'), fit: BoxFit.cover)),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("AstraZeneca",
+                style: TextStyle(
+                    color: Colors.white, decoration: TextDecoration.none)),
+            Icon(Icons.remove_done,color: Colors.orange,size: 48,)
+          ],
+        ),
+      ),
+    );
+  }
 }
