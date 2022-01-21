620fdb432f4492098384ec5df1b796a5a5272f8a
admin <admin@admindeMacBook-Air-3.local>
2022-01-21 17:50:44 +0800

    feat: add bugly init

--- bugly/lib/main.dart
+++ bugly/lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bugly/flutter_bugly.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => FlutterBugly.postCatchedException(() => runApp(const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -48,6 +48,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _platformVersion = 'Unknown';
+  // GlobalKey<UpdateDialogState>
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBugly.init(
+      androidAppId: 'adeeed5ec1',
+      iOSAppId: '0941ecf9a6',
+      customUpgrade: true,
+    ).then((_result) {
+      setState(() {
+        _platformVersion = _result.message;
+        print(_result.appId);
+      });
+    });
+
+    FlutterBugly.onCheckUpgrade.listen((_upgradeInfo) {
+      print('展示升级弹窗');
+    });
+
+    FlutterBugly.setUserId('6666666');
+    FlutterBugly.putUserData(key: 'key', value: 'value');
+    int tag = 9527;
+    FlutterBugly.setUserTag(tag);
+  }
+
+  @override
+  void dispose() {
+    FlutterBugly.dispose();
+    super.dispose();
+  }
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -57,52 +90,42 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter+=2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: GestureDetector(
+        onTap: () {
+          print('1312');
+
+          FlutterBugly.setUserId('7777');
+          FlutterBugly.putUserData(key: 'key1', value: 'value1');
+          int tag = 9528;
+          FlutterBugly.setUserTag(tag);
+          // if (Platform.isAndroid) {
+            _checkUpgrade();
+          // }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text('init result: $_platformVersion\n'),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -111,5 +134,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _checkUpgrade() {
+    print('获取更新中');
+    FlutterBugly.checkUpgrade();
   }
 }
